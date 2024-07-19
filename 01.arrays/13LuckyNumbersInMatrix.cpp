ab4102785928c8c281fa65c04b1768169e50b4cd
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-07-19 10:35:10 +0530

    commit

--- 01.arrays/13LuckyNumbersInMatrix.cpp
+++ 01.arrays/13LuckyNumbersInMatrix.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+// Given an n x m matrix of distinct numbers, return all lucky numbers in the matrix in any order.
 // A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
 
 // Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
